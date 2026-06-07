4a0a9614234e928fcae01c422c4b548b27e373ac
faayoub <fakiayoub71@gmail.com>
2026-06-07 15:26:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,4 @@ if __name__ == "__main__":
         uvicorn.run(app="main:api.app", host="0.0.0.0", port=config.PORT)
     except Exception as e:
         print(f"启动服务器时发生错误: {e}")
+        app = api
